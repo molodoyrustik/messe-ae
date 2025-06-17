4a4d98603e8ba90248f7d90433dc571841315863
neolite <ufadiz@gmail.com>
2025-06-17 19:14:47 +0300

    remove gatsby

--- gatsby-browser.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { Providers } from "./src/components/Providers";
-import type { GatsbyBrowser } from "gatsby";
-
-export const wrapRootElement: GatsbyBrowser["wrapRootElement"] = ({
-  element,
-}) => {
-  return <Providers>{element}</Providers>;
-};
--- gatsby-ssr.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { Providers } from "./src/components/Providers";
-import type { GatsbySSR } from "gatsby";
-
-export const wrapRootElement: GatsbySSR["wrapRootElement"] = ({ element }) => {
-  return <Providers>{element}</Providers>;
-};
