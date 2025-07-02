8917022813726bfc2bcfdd8ea0cabc2c223cc56d
neolite <ufadiz@gmail.com>
2025-07-02 11:05:48 +0300

    Fix margins

--- src/components/ArticlePageClient.tsx
+++ src/components/ArticlePageClient.tsx
@@ -124,7 +124,7 @@ export default function ArticlePageClient({ articleData, relatedArticles }: Arti
 
       {/* Article Content */}
       <Container maxWidth="xl" sx={{ pt: { xs: '3rem', md: '3.75rem' }, pb: { xs: '3rem', md: '6rem' }, px: { xs: '1rem', md: 0 } }}>
-        <Box>
+      <Box sx={{ px: { xs: '1rem', md: '2.5rem' } }}>
           <Box sx={{ display: 'flex', gap: { xs: 0, md: '5rem' }, flexDirection: { xs: 'column', md: 'row' }, alignItems: 'flex-start', justifyContent: 'space-between' }}>
             {/* Article Body */}
             <Box
